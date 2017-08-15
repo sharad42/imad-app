9a02dd44025b3844dabde7e4eb9b6231f7477760
Sharad Pandey <sharadpandey42@gmail.com>
2017-08-15 13:35:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,6 @@ button.onclick = function () {
      
  };  
  
- request.open('GET','http://http://sharadpandey42.imad.hasura-app.io/counter',true);
+ request.open('GET','http://sharadpandey42.imad.hasura-app.io/counter',true);
  request.send(null);
 }
