f40079e7843b690a33a3c58ae107d3b3fdcf7392
Sharad Pandey <sharadpandey42@gmail.com>
2017-08-15 13:29:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
-console.log('Loaded!');
+//Counter Code
+var button = document.getElementById("counter");
+
+button.onclick = functon(){
+    
+  var request = new XMLHttpRequest();
+  
+ request.onreadystatechange = function (){
+   if(request.readystate === XMLHttpRequest.DONE){
+      if(request.status === 200){
+          var counter = request.responseText;
+          var span = document.getElementById("count");
+          span.innerHTML = counter.toString();
+          
+      }
+   }
+     
+ };  
+ 
+ request.open('GET','http://http://sharadpandey42.imad.hasura-app.io/counter',true);
+ request.send(null);
+}
